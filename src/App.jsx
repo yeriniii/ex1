fa8b0d7ed61eb13267dab061a4cffce4ba01bb49
yeriniii <yerini000303@naver.com>
2024-02-07 00:48:07 +0900

    ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-
+import { plusN } from "./redux/modules/calculator";
+import { minusN } from "./redux/modules/calculator";
+import { useState } from "react";
 function App() {
+  const [number, setNumber] = useState(0);
+  const dispatch = useDispatch();
+  const result = useSelector((state) => state.calculator.number);
+
+  const plusHandler = () => {
+    dispatch(plusN(number));
+  };
+  const minusHandler = () => {
+    dispatch(minusN(number));
+  };
   return (
     <div className="App">
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input value={number} onChange={(e) => setNumber(+e.target.value)} />{" "}
+        만큼을 <button onClick={plusHandler}>더할게요</button>{" "}
+        <button onClick={minusHandler}>뺄게요</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{result}</p>
       </div>
     </div>
   );
--- src/redux/modules/calculator.js
+++ src/redux/modules/calculator.js
@@ -1,10 +1,40 @@
+const PLUS_N = "PLUS_N";
+const MINUS_N = "MINUS_N";
+
+export const plusN = (payload) => {
+  return {
+    type: PLUS_N,
+    payload,
+  };
+};
+export const minusN = (payload) => {
+  return {
+    type: MINUS_N,
+    payload,
+  };
+};
+
 // 초기 상태값
 const initialState = {
   number: 0,
 };
 
 // 리듀서
-const calculator = (state = initialState, action) => {};
+const calculator = (state = initialState, action) => {
+  switch (action.type) {
+    case PLUS_N:
+      return {
+        number: state.number + action.payload,
+      };
+
+    case MINUS_N:
+      return {
+        number: state.number - action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 // 모듈파일에서는 리듀서를 export default 한다.
 export default calculator;
